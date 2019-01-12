fed5c3a9e8fd4fc351e66392df7954720768b462
Arnaud Banvillet <arnaud@matierenoire.io>
2019-01-12 13:27:00 +0100

    Date filter avec heure
    Sinon on ne récupére pas les statistiques du jours même et pas les permanences qui tombe le dernier jours du mois

--- frontend/src/utils/Api.js
+++ frontend/src/utils/Api.js
@@ -16,13 +16,13 @@ const Api = {
     "order[date]" : "ASC",
     composter: selectedComposter,
     "date[after]": `${currentMonth}-01`,
-    "date[before]": `${currentMonth}-31`
+    "date[before]": `${currentMonth}-31T23:59:00`
   }),
   getStatsByComposter: ( selectedComposter ) => ApiCall.get( 'permanences',{
     "order[date]" : "ASC",
     composter: selectedComposter,
-    "date[after]": moment().subtract( 1, 'years').format( 'YYYY-MM-DD'),
-    "date[before]": moment().format( 'YYYY-MM-DD')
+    "date[after]": moment().subtract( 1, 'years').format(),
+    "date[before]": moment().format()
   })
 }
 
