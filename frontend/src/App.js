536326192d492c3adff27e0cfa0a06a981f68efd
Arnaud Banvillet <arnaud@matierenoire.io>
2019-03-19 20:58:18 +0100

    Creat react app update + Prettier

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,115 +4,139 @@
  * We recommend including the built version of this JavaScript file
  * (and its CSS file) in your base layout (base.html.twig).
  */
-import './App.css';
+import './App.css'
 
-import React, {Fragment} from "react"
+import React, { Fragment } from 'react'
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles'
 import CssBaseline from '@material-ui/core/CssBaseline'
-import {lightGreen, blue} from '@material-ui/core/colors'
-import { AppBar, Toolbar, Typography, IconButton, Tooltip } from '@material-ui/core'
+import { lightGreen, blue } from '@material-ui/core/colors'
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  IconButton,
+  Tooltip
+} from '@material-ui/core'
 import HelpPage from './component/HelpPage'
 import Composter from './component/Composter'
-import { AppContext } from "./app-context"
+import { AppContext } from './app-context'
 import AccountCircle from '@material-ui/icons/AccountCircle'
 import Help from '@material-ui/icons/Help'
-import Api from "./utils/Api"
+import Api from './utils/Api'
 
-import { ReactComponent as Logo } from './svg/logo.svg';
+import { ReactComponent as Logo } from './svg/logo.svg'
 
-
-const defaultTheme = createMuiTheme();
+const defaultTheme = createMuiTheme()
 
 const theme = createMuiTheme({
-    typography: {
-        useNextVariants: true,
+  typography: {
+    useNextVariants: true
+  },
+  palette: {
+    primary: lightGreen,
+    blue: blue
+  },
+  overrides: {
+    MuiChip: {
+      root: {
+        marginTop: defaultTheme.spacing.unit,
+        marginRight: defaultTheme.spacing.unit
+      }
     },
-    palette: {
-        primary: lightGreen,
-        blue: blue
-    },
-    overrides: {
-        MuiChip: {
-            root: {
-                marginTop: defaultTheme.spacing.unit,
-                marginRight: defaultTheme.spacing.unit,
-            }
-        },
-        MuiPaper :{
-            root: {
-                textAlign: "center",
-                height: "100%"
-            }
-        }
+    MuiPaper: {
+      root: {
+        textAlign: 'center',
+        height: '100%'
+      }
     }
-});
+  }
+})
 
 class App extends React.Component {
+  constructor(props) {
+    super(props)
 
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            selectedComposter: null,
-            changeComposter: ( composterId ) => this.setContextComposter( composterId )
-        };
+    this.state = {
+      selectedComposter: null,
+      changeComposter: composterId => this.setContextComposter(composterId)
     }
+  }
 
-    async setContextComposter( composterId ){
+  async setContextComposter(composterId) {
+    this.setState({
+      selectedComposter: null
+    })
 
-        this.setState({
-            selectedComposter: null
-        })
-
-        const selectedComposter = await Api.getComposter( composterId )
-        if( selectedComposter.status === 200 ){
-            this.setState({
-                selectedComposter: selectedComposter.data
-            })
-
-        }
+    const selectedComposter = await Api.getComposter(composterId)
+    if (selectedComposter.status === 200) {
+      this.setState({
+        selectedComposter: selectedComposter.data
+      })
     }
+  }
 
-    componentDidMount(){
-        this.setContextComposter( '1' )
-    }
+  componentDidMount() {
+    this.setContextComposter('1')
+  }
 
-    render() {
-
-        return (
-            <AppContext.Provider value={this.state}>
-                <MuiThemeProvider theme={theme}>
-                    <CssBaseline />
-                    <Router>
-                        <Fragment>
-                            <AppBar position="static">
-                                <Toolbar>
-                                    <Typography variant="h6" color="inherit" style={{flexGrow:"1", textAlign: "left", textDecoration:'none'}} component={Link} to="/">
-                                        <Logo style={{width:50,height:50,verticalAlign:'middle', marginRight:'.5em'}}/>
-                                        <span>Compost'heur</span>
-                                    </Typography>
-                                    <IconButton color="inherit" component={Link} to="/aide/">
-                                        <Help color="inherit"/>
-                                    </IconButton>
-                                    <Tooltip title="Bientôt ;-)">
-                                        <IconButton color="inherit">
-                                            <AccountCircle color="inherit"/>
-                                        </IconButton>
-                                    </Tooltip>
-                                </Toolbar>
-                            </AppBar>
-                            <div style={{maxWidth: "75em", margin:"2em auto", padding:"0 7%", boxSizing:"content-box"}}>
-                                <Route path="/" exact component={Composter} />
-                                <Route path="/aide/" component={HelpPage} />
-                            </div>
-                        </Fragment>
-                    </Router>
-                </MuiThemeProvider>
-            </AppContext.Provider>
-        );
-    }
+  render() {
+    return (
+      <AppContext.Provider value={this.state}>
+        <MuiThemeProvider theme={theme}>
+          <CssBaseline />
+          <Router>
+            <Fragment>
+              <AppBar position="static">
+                <Toolbar>
+                  <Typography
+                    variant="h6"
+                    color="inherit"
+                    style={{
+                      flexGrow: '1',
+                      textAlign: 'left',
+                      textDecoration: 'none'
+                    }}
+                    component={Link}
+                    to="/"
+                  >
+                    <Logo
+                      style={{
+                        width: 50,
+                        height: 50,
+                        verticalAlign: 'middle',
+                        marginRight: '.5em'
+                      }}
+                    />
+                    <span>Compost'heur</span>
+                  </Typography>
+                  <IconButton color="inherit" component={Link} to="/aide/">
+                    <Help color="inherit" />
+                  </IconButton>
+                  <Tooltip title="Bientôt ;-)">
+                    <IconButton color="inherit">
+                      <AccountCircle color="inherit" />
+                    </IconButton>
+                  </Tooltip>
+                </Toolbar>
+              </AppBar>
+              <div
+                style={{
+                  maxWidth: '75em',
+                  margin: '2em auto',
+                  padding: '0 7%',
+                  boxSizing: 'content-box'
+                }}
+              >
+                <Route path="/" exact component={Composter} />
+                <Route path="/aide/" component={HelpPage} />
+              </div>
+            </Fragment>
+          </Router>
+        </MuiThemeProvider>
+      </AppContext.Provider>
+    )
+  }
 }
-
 
 export default App
--- frontend/src/component/Composter.js
+++ frontend/src/component/Composter.js
@@ -1,79 +1,82 @@
-import React, {Fragment} from "react"
-import PermancesList from './PermancesList';
-import { AppContext } from '../app-context';
-import ComposterStats from './ComposterStats';
-import {Tabs, Tab, Typography, LinearProgress} from "@material-ui/core"
+import React, { Fragment } from 'react'
+import PermancesList from './PermancesList'
+import { AppContext } from '../app-context'
+import ComposterStats from './ComposterStats'
+import { Tabs, Tab, Typography, LinearProgress } from '@material-ui/core'
 import Markdown from '../utils/Markdown'
 
 class ComposterPermanencesListe extends React.Component {
+  constructor(props) {
+    super(props)
 
-    constructor( props ) {
-        super( props );
-
-        this.state = {
-            openedTab: 0,
-        };
+    this.state = {
+      openedTab: 0
     }
+  }
 
-    handleChange(event, openedTab) {
-        this.setState({ openedTab });
-    };
+  handleChange(event, openedTab) {
+    this.setState({ openedTab })
+  }
 
-    render() {
-        let { openedTab } = this.state;
-        let appContext = this.context;
+  render() {
+    let { openedTab } = this.state
+    let appContext = this.context
 
-        return (
-            <Fragment>
-                { appContext.selectedComposter ?
-                    <Fragment>
+    return (
+      <Fragment>
+        {appContext.selectedComposter ? (
+          <Fragment>
+            <Typography component="h1" variant="h3" gutterBottom>
+              {appContext.selectedComposter.name}
+            </Typography>
 
-                        <Typography component="h1" variant="h3" gutterBottom>{appContext.selectedComposter.name}</Typography>
+            <Tabs
+              value={openedTab}
+              onChange={(event, openedTab) =>
+                this.handleChange(event, openedTab)
+              }
+              indicatorColor="primary"
+              style={{ marginBottom: '2em' }}
+            >
+              <Tab label="Permanences" />
+              <Tab label="Informations" />
+              <Tab label="Stats" />
+            </Tabs>
 
-                        <Tabs
-                            value={openedTab}
-                            onChange={ ( event, openedTab ) => this.handleChange( event, openedTab )}
-                            indicatorColor="primary"
-                            style={{marginBottom:'2em'}}
-                        >
-                            <Tab label="Permanences" />
-                            <Tab label="Informations" />
-                            <Tab label="Stats" />
-                        </Tabs>
+            {openedTab === 0 && (
+              <Fragment>
+                <Markdown
+                  source={appContext.selectedComposter.short_description}
+                />
 
-                        { openedTab === 0 &&
-                            <Fragment>
-                                <Markdown source={appContext.selectedComposter.short_description} />
-
-                                <PermancesList />
-                            </Fragment>
-
-                        }
-                        { openedTab === 1 &&
-                            <Fragment>
-                                { appContext.selectedComposter.address &&
-                                    <Fragment>
-                                        <Typography component="h2" variant="h6">Adresse du composter</Typography>
-                                        <Markdown source={appContext.selectedComposter.address} />
-                                    </Fragment>
-                                }
-                                <Typography component="h2" variant="h6">Informations</Typography>
-                                <Markdown source={appContext.selectedComposter.description} />
-                            </Fragment>
-                        }
-                        { openedTab === 2 &&
-                            <ComposterStats />
-                        }
-
-                    </Fragment>
-                    :
-                    <LinearProgress/>
-                }
-            </Fragment>
-
-        )
-    }
+                <PermancesList />
+              </Fragment>
+            )}
+            {openedTab === 1 && (
+              <Fragment>
+                {appContext.selectedComposter.address && (
+                  <Fragment>
+                    <Typography component="h2" variant="h6">
+                      Adresse du composter
+                    </Typography>
+                    <Markdown source={appContext.selectedComposter.address} />
+                  </Fragment>
+                )}
+                <Typography component="h2" variant="h6">
+                  Informations
+                </Typography>
+                <Markdown source={appContext.selectedComposter.description} />
+              </Fragment>
+            )}
+            {openedTab === 2 && <ComposterStats />}
+          </Fragment>
+        ) : (
+          <LinearProgress />
+        )}
+      </Fragment>
+    )
+  }
 }
-ComposterPermanencesListe.contextType = AppContext;
+ComposterPermanencesListe.contextType = AppContext
 
 export default ComposterPermanencesListe
